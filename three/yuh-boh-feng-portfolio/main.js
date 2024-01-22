97ced965c9bdb382af34e68de677de57a5ae5ebf
Yuh-Boh Feng <fengyuhboh@gmail.com>
2024-01-22 12:15:12 -0800

    testing multiple planes

--- three/yuh-boh-feng-portfolio/main.js
+++ three/yuh-boh-feng-portfolio/main.js
@@ -1,63 +1,37 @@
+//Hengs example:
+
+// plane1 = scene.getObjectByName("Name of your plane")
+// plane1.position.set(0, 1, 0); //(x,y,z)
+
 import * as THREE from "three";
-import { GUI } from "dat.gui";
+// import { GUI } from "dat.gui";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-import gsap from "gsap";
+
 
 // 1 - CREATING PLANE
-
-const planeMaterial = new THREE.MeshPhongMaterial({
+const planeMaterial = new THREE.MeshStandardMaterial({
+  color: 0x00ff00,
   side: THREE.DoubleSide, // color side red, color both sides red.
-  flatShading: true,
-  vertexColors: true,
+  // flatShading: true,
 });
-const planeGeometry = new THREE.PlaneGeometry(500, 500, 50, 50); // width, height, width segment, height segment
-const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
-const planeParameters = planeMesh.geometry.parameters;
 
-// CREATE PLANE
-function generatePlane() {
-  planeMesh.geometry.dispose();
-  planeMesh.geometry = new THREE.PlaneGeometry(
-    planeParameters.width,
-    planeParameters.height,
-    planeParameters.widthSegments,
-    planeParameters.heightSegments
-  );
 
-  // vertice position randomization
-  const { array } = planeMesh.geometry.attributes.position;
-  const randomValues = [];
-  for (let i = 0; i < array.length; i++) {
-    if (i % 3 === 0) {
-      const x = array[i];
-      const y = array[i + 1];
-      const z = array[i + 2];
 
-      array[i] = x + (Math.random() - 0.5) * 3;
-      array[i + 1] = y + (Math.random() - 0.5) * 3;
-      array[i + 2] = z + (Math.random() - 0.5) * 3;
-    }
-    randomValues.push(Math.random() * Math.PI * 2);
-  }
-  planeMesh.geometry.attributes.position.randomValues = randomValues;
+const planeGeometry = new THREE.PlaneGeometry(400, 400); // width, height, width segment, height segment
 
-  planeMesh.geometry.attributes.position.originalPosition =
-    planeMesh.geometry.attributes.position.array;
+//Create you 6 different planes:
+const planeFrontMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+const planeBackMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+const planeLeftMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+const planeRightMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+const planeTopMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+const planeBottomMesh = new THREE.Mesh(planeGeometry, planeMaterial);
 
-  // UPDATE PLANE COLOR = PLANE COLOR
-  const colors = [];
-  for (let i = 0; i < planeMesh.geometry.attributes.position.count; i++) {
-    colors.push(0, 0, 0); // default plane color
-  }
 
-  planeMesh.geometry.setAttribute(
-    "color",
-    new THREE.BufferAttribute(new Float32Array(colors), 3)
-  );
-}
 
-const raycaster = new THREE.Raycaster();
 const scene = new THREE.Scene();
+
+
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
@@ -73,10 +47,36 @@ document.body.appendChild(renderer.domElement);
 
 // CAMERA CONTROL
 new OrbitControls(camera, renderer.domElement);
-camera.position.z = 150; // how far away from center of 3D Model
+camera.position.z = 5; // how far away from center of 3D Model
 
-scene.add(planeMesh);
-generatePlane();
+
+//IMPORTANT THIS IS WHERE YOU ADD ALL YOUR PLANE MESHES!!!
+//ADD 6 PLANES HEREE!!
+scene.add(planeFrontMesh, planeBackMesh, planeLeftMesh, planeRightMesh, planeTopMesh, planeBottomMesh);
+
+//Now all the planes should show up in your scene.
+//And you now have access to each plane in your scene.
+//NOW YOU NEED TO CHANGE THEIR POSITIONS/ ROTATIONS TO GET CREATE THE SHAPE OF A CUBE!
+planeFrontMesh.position.set(0, 0, 250); //(x,y,z)
+planeBackMesh.position.set(0, 0, -250); //(x,y,z)
+planeLeftMesh.position.set( -250, 0, 0); //(x,y,z)
+planeRightMesh.position.set(250, 0, 0); //(x,y,z)
+planeTopMesh.position.set(0, 250, 0); //(x,y,z)
+planeBottomMesh.position.set(0, -250, 0); //(x,y,z)
+
+//Create vector 3:
+// const a = new THREE.Vector3( -250, 500, 0 );
+// // const b = new THREE.Vector3( 250, 250, 0 );
+// // const d = a.distanceTo( b );
+// planeTopMesh.rotateOnAxis(a, 45.0);
+
+
+//Change the rotation of the top and bottom planes
+// planeTopMesh = new THREE.Euler(0, 1, 1.57, "XYZ");
+// planeBottomMesh.rotation.x = Math.PI / 2;
+// planeLeftMesh.rotation.y = Math.PI / 2;
+// planeRightMesh.rotation.y = Math.PI / 2;
+
 
 // CREATING LIGHT/BACKLIGHT
 const light = new THREE.DirectionalLight(0xffffff, 1); // white, max brightness;
@@ -87,109 +87,19 @@ const backlight = new THREE.DirectionalLight(0xffffff, 1); // white, max brightn
 backlight.position.set(0, 0, -1); // x, y, in front of object;
 scene.add(backlight);
 
-const mouse = {
-  x: undefined,
-  y: undefined,
-};
+// const mouse = {
+//   x: undefined,
+//   y: undefined,
+// };
 
 // ANIMATE MESH
-let frame = 0;
+// let frame = 0;
 
 function animate() {
   requestAnimationFrame(animate); // animation calls on itself
   renderer.render(scene, camera); // animate now
-  raycaster.setFromCamera(mouse, camera);
-  frame += 0.01;
+  // frame += 0.01;
 
-  const { array, originalPosition, randomValues } =
-    planeMesh.geometry.attributes.position;
-  for (let i = 0; i < array.length; i += 3) {
-    // x
-    array[i] = originalPosition[i] + Math.cos(frame + randomValues[i]) * 0.03;
-    // y
-    array[i + 1] =
-      originalPosition[i + 1] + Math.sin(frame + randomValues[i + 1]) * 0.03;
-  }
-
-  planeMesh.geometry.attributes.position.needsUpdate = true;
-
-  const intersects = raycaster.intersectObject(planeMesh);
-  if (intersects.length > 0) {
-    const { color } = intersects[0].object.geometry.attributes;
-
-    // vertice 1
-    color.setX(intersects[0].face.a, 0.1);
-    color.setY(intersects[0].face.a, 0.5);
-    color.setZ(intersects[0].face.a, 1);
-
-    // vertice 2
-    color.setX(intersects[0].face.b, 0.1);
-    color.setY(intersects[0].face.b, 0.5);
-    color.setZ(intersects[0].face.b, 1);
-
-    // vertice 3
-    color.setX(intersects[0].face.c, 0.1);
-    color.setY(intersects[0].face.c, 0.5);
-    color.setZ(intersects[0].face.c, 1);
-
-    intersects[0].object.geometry.attributes.color.needsUpdate = true;
-
-    const initialColor = {
-      r: 0,
-      g: 0,
-      b: 0,
-    };
-
-    const hoverColor = {
-      r: 1,
-      g: 1,
-      b: 1,
-    };
-
-    gsap.to(hoverColor, {
-      r: initialColor.r,
-      g: initialColor.g,
-      b: initialColor.b,
-      duration: 1,
-      onUpdate: () => {
-        // vertice 1
-        color.setX(intersects[0].face.a, hoverColor.r);
-        color.setY(intersects[0].face.a, hoverColor.g);
-        color.setZ(intersects[0].face.a, hoverColor.b);
-
-        // vertice 2
-        color.setX(intersects[0].face.b, hoverColor.r);
-        color.setY(intersects[0].face.b, hoverColor.g);
-        color.setZ(intersects[0].face.b, hoverColor.b);
-
-        // vertice 3
-        color.setX(intersects[0].face.c, hoverColor.r);
-        color.setY(intersects[0].face.c, hoverColor.g);
-        color.setZ(intersects[0].face.c, hoverColor.b);
-        color.needsUpdate = true;
-      },
-    }); // .to() takes classes or objects
-  }
 }
 
-// CREATE DAT.GUI
-const gui = new GUI();
-const planeFolder = gui.addFolder("Plane"); // create menu
-planeFolder.open(); // default open menu
-
-planeFolder.add(planeParameters, "width", 1, 500).onChange(generatePlane); // adds category
-planeFolder.add(planeParameters, "height", 1, 500).onChange(generatePlane);
-planeFolder
-  .add(planeParameters, "widthSegments", 1, 100)
-  .onChange(generatePlane);
-planeFolder
-  .add(planeParameters, "heightSegments", 1, 100)
-  .onChange(generatePlane);
-
-
 animate();
-
-addEventListener("mousemove", (event) => {
-  mouse.x = (event.clientX / innerWidth) * 2 - 1;
-  mouse.y = -(event.clientY / innerHeight) * 2 + 1;
-});
