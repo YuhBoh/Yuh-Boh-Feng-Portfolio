1166e2531a10dee499b0b7d83a4677d9d052e931
Yuh-Boh Feng <fengyuhboh@gmail.com>
2024-01-24 19:57:55 -0800

    added another side

--- three/yuh-boh-feng-portfolio/src/main.js
+++ three/yuh-boh-feng-portfolio/src/main.js
@@ -2,7 +2,7 @@ import * as THREE from "three";
 import { GUI } from "dat.gui";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import gsap from "gsap";
-import { forwardMesh } from "./forwardMesh";
+import { forwardMesh } from "./mesh-sides/forwardMesh";
 
 const scene = new THREE.Scene();
 const frontMesh = forwardMesh();
