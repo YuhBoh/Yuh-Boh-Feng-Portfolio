e08dd52236ed334c8f393198c6fe0d44389d14ef
Yuh-Boh Feng <fengyuhboh@gmail.com>
2024-01-25 22:05:33 -0800

    added right plane

--- three/yuh-boh-feng-portfolio/main.js
+++ three/yuh-boh-feng-portfolio/main.js
@@ -25,6 +25,8 @@ const leftMesh = frontMesh.clone();
 const leftParameters = leftMesh.geometry.parameters;
 
 // RIGHT PLANE
+const rightMesh = frontMesh.clone();
+const rightParameters = rightMesh.geometry.parameters;
 
 // TOP PLANE
 
@@ -163,6 +165,51 @@ function generateLeftPlane() {
   );
 }
 
+// RIGHT PLANE GEOMETRY/POSITION
+function generateRightPlane() {
+  rightMesh.geometry.dispose();
+  rightMesh.geometry = new THREE.PlaneGeometry(
+    rightParameters.width,
+    rightParameters.height,
+    rightParameters.widthSegments,
+    rightParameters.heightSegments
+  );
+
+  rightMesh.position.set(250, 0, 0); //(x,y,z)
+  rightMesh.rotateY(Math.PI / 2);
+
+  // vertice position randomization
+  const { array } = rightMesh.geometry.attributes.position;
+  const randomValues = [];
+  for (let i = 0; i < array.length; i++) {
+    if (i % 3 === 0) {
+      const x = array[i];
+      const y = array[i + 1];
+      const z = array[i + 2];
+
+      array[i] = x + (Math.random() - 0.5) * 3;
+      array[i + 1] = y + (Math.random() - 0.5) * 3;
+      array[i + 2] = z + (Math.random() - 0.5) * 3;
+    }
+    randomValues.push(Math.random() * Math.PI * 2);
+  }
+  rightMesh.geometry.attributes.position.randomValues = randomValues;
+
+  rightMesh.geometry.attributes.position.originalPosition =
+    rightMesh.geometry.attributes.position.array;
+
+  // UPDATE PLANE COLOR = PLANE COLOR
+  const colors = [];
+  for (let i = 0; i < rightMesh.geometry.attributes.position.count; i++) {
+    colors.push(0, 0, 1); // default right color
+  }
+
+  rightMesh.geometry.setAttribute(
+    "color",
+    new THREE.BufferAttribute(new Float32Array(colors), 3)
+  );
+};
+
 const raycaster = new THREE.Raycaster();
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -186,9 +233,10 @@ camera.position.z = 150; // how far away from center of 3D Model
 generateFrontPlane();
 generateBackPlane();
 generateLeftPlane();
+generateRightPlane();
 
 // RENDER PLANES
-scene.add(frontMesh, leftMesh, backMesh);
+scene.add(frontMesh, leftMesh, backMesh, rightMesh);
 
 // CREATING/CALLING LIGHT
 function light() {
@@ -455,6 +503,84 @@ function leftAnimate() {
   }
 }
 
+// ANIME RIGHT PLANE
+function rightAnimate() {
+  requestAnimationFrame(rightAnimate); // animation calls on itself
+  renderer.render(scene, camera); // animate now
+  raycaster.setFromCamera(mouse, camera);
+  frame += 0.01;
+
+  const { array, originalPosition, randomValues } =
+    rightMesh.geometry.attributes.position;
+  for (let i = 0; i < array.length; i += 3) {
+    // x
+    array[i] = originalPosition[i] + Math.cos(frame + randomValues[i]) * 0.03;
+    // y
+    array[i + 1] =
+      originalPosition[i + 1] + Math.sin(frame + randomValues[i + 1]) * 0.03;
+  }
+
+  rightMesh.geometry.attributes.position.needsUpdate = true;
+
+  const intersects = raycaster.intersectObject(rightMesh);
+  if (intersects.length > 0) {
+    const { color } = intersects[0].object.geometry.attributes;
+
+    // vertice 1
+    color.setX(intersects[0].face.a, 0.1);
+    color.setY(intersects[0].face.a, 0.5);
+    color.setZ(intersects[0].face.a, 1);
+
+    // vertice 2
+    color.setX(intersects[0].face.b, 0.1);
+    color.setY(intersects[0].face.b, 0.5);
+    color.setZ(intersects[0].face.b, 1);
+
+    // vertice 3
+    color.setX(intersects[0].face.c, 0.1);
+    color.setY(intersects[0].face.c, 0.5);
+    color.setZ(intersects[0].face.c, 1);
+
+    intersects[0].object.geometry.attributes.color.needsUpdate = true;
+
+    const initialColor = {
+      r: 0,
+      g: 0,
+      b: 0,
+    };
+
+    const hoverColor = {
+      r: 1,
+      g: 1,
+      b: 1,
+    };
+
+    gsap.to(hoverColor, {
+      r: initialColor.r,
+      g: initialColor.g,
+      b: initialColor.b,
+      duration: 1,
+      onUpdate: () => {
+        // vertice 1
+        color.setX(intersects[0].face.a, hoverColor.r);
+        color.setY(intersects[0].face.a, hoverColor.g);
+        color.setZ(intersects[0].face.a, hoverColor.b);
+
+        // vertice 2
+        color.setX(intersects[0].face.b, hoverColor.r);
+        color.setY(intersects[0].face.b, hoverColor.g);
+        color.setZ(intersects[0].face.b, hoverColor.b);
+
+        // vertice 3
+        color.setX(intersects[0].face.c, hoverColor.r);
+        color.setY(intersects[0].face.c, hoverColor.g);
+        color.setZ(intersects[0].face.c, hoverColor.b);
+        color.needsUpdate = true;
+      },
+    }); // .to() takes classes or objects
+  }
+}
+
 // // CREATE DAT.GUI
 // const gui = new GUI();
 // const leftFolder = gui.addFolder("Plane"); // create menu
@@ -474,6 +600,7 @@ function leftAnimate() {
 frontAnimate();
 backAnimate();
 leftAnimate();
+rightAnimate();
 
 addEventListener("mousemove", (event) => {
   mouse.x = (event.clientX / innerWidth) * 2 - 1;
